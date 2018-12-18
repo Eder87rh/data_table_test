58690e4b5a47fcde1da88c89146b7861df9c323f
Eder Ramirez <documentador1@telecomo.com.mx>
2018-12-18 17:50:20 -0600

    Passing data to vuex store

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -8,7 +8,7 @@
                 <th>Date</th>
                 <th>Amount</th>
             </tr>
-            <tr v-for="data in data" :key="data.id">
+            <tr v-for="data in $store.state.data" :key="data.id">
                 <td>{{ data.id }}</td>
                 <td>{{ data.name }}</td>
                 <td>{{ data.description }}</td>
@@ -23,22 +23,6 @@
 export default {
     data(){
         return{
-            data:[
-                {
-                    id: '3471DA17-401F-9633-BF81-4CADA6FD5C79',
-                    name: 'Kyra Lester',
-                    description: 'Curabitur dictum. Phasellus in',
-                    date: '2017-07-23T04:24:49-07:00',
-                    amount: '345.54'
-                },
-                {
-                    id: '3471DA17-401F-9633-BF81-4CADA6FD5C79',
-                    name: 'Kyra Lester',
-                    description: 'Curabitur dictum. Phasellus in',
-                    date: '2017-07-23T04:24:49-07:00',
-                    amount: '345.54'
-                }
-            ]
         }
     },
     methods:{
--- src/store.js
+++ src/store.js
@@ -5,12 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    data:[
+      {
+        id: '3471DA17-401F-9633-BF81-4CADA6FD5C79',
+        name: 'Kyra Lester',
+        description: 'Curabitur dictum. Phasellus in',
+        date: '2017-07-23T04:24:49-07:00',
+        amount: '345.54'
+      },
+      {
+          id: '3471DA17-401F-9633-BF81-4CADA6FD5C79',
+          name: 'Kyra Lester',
+          description: 'Curabitur dictum. Phasellus in',
+          date: '2017-07-23T04:24:49-07:00',
+          amount: '345.54'
+      }
+    ]
   },
   mutations: {
 
   },
   actions: {
-
+    
   }
 })
